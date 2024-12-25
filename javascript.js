74b045636451f79fea336736de5fd539a9790998
Viktoria Dyakova <viktoria.guliants@yandex.ru>
2024-12-25 18:16:05 +0300

    second commit

--- javascript.js
+++ javascript.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     button.addEventListener('click', function () {
       // Действие при нажатии кнопки (например, переход на другую страницу)
-      window.location.href = `http://127.0.0.1:5500/question.html?id=${id}`;
+      window.location.href = `https://viktoria3002.github.io/Quiz/question.html?id=${id}`;
 
       // Сохраняем состояние кнопки как посещенной
       localStorage.setItem(`buttonVisited_${id}`, true);
